b35ea8f87e9212df23f1f067014e21fdcd99d3c1
Joseph Elliott <josephelliott127@gmail.com>
2026-03-21 21:39:59 -0400

    feat: export v2 means test engine

---
+++ src/engine/v2/index.ts
@@ -0,0 +1,12 @@
+export { runMeansTestV2 } from "./meansTestV2";
+export { normalizeLegacyInput } from "./normalize";
+export type {
+  MeansTestInputV2,
+  MeansTestResultV2,
+  CalculationAudit,
+  LineItemV2,
+  VehicleInput,
+  IncomeSourceV2,
+  DatasetVersionMeta,
+  DataSourceKind,
+} from "./types";
